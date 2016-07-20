c7365d7ef1b38db8113a639a44abf6066beb5134
Carey Li <careyliau@gmail.com>
2016-07-21 03:44:48 +1000

    Add package.json

--- test/test.js
+++ test/test.js
@@ -5,7 +5,8 @@ var assert = require('chai').assert,
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) { var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8); return v.toString(16); });
     },
     test_entries = [],
-    false_entries = [];
+    false_entries = [],
+    importData = "AwGl7SOrZ+6IcpqXrZj2u5/vQg4o0k8syi6q2m+uxh5p1l9tzj7r3n/voIHChokeLGSJ0qbJny5ihcqWqV6tZo3atunfr2GDxo6ZPmzli9au2b9u44fOnrl+7eeP3r75/+/QIDgoNCQ8LDIiOio2Jj4uMSE5KTUlPS0zIzsrNyc/LzCguKi0pLyssqK6qramvq6xobmptaW9rbOju6u3p7+vsGB4aHRkfGxyYnpqdmZ+bnFheWl1ZX1tc2N7a3dnf29w4OZgEYj8+OLq8ub67vbh/unx5fnt9eP96/Pn++/34B/yBgJBwLBoIh4KhkJh0LhsIR8KRiJRyLRqIx6KxmJx2LxuIJ+KJhJJxLJpIp5KplJp1LptIZ9KZjJZzLZrI57K5nJ53L5vIF/KFgpFwrFool4qlkpl0rlsoV8qVvzOirVyo16q1mp12r1uoN+qNhpNxrNpot5qtlpt1rttod9qdjpdzrdro97q9qVVnr93oD/qDgZDwbDoYj4ajNF9kbj0YT8aTiZTybTqYz6azmZz2bzuYL+aLhZLxbLpYr5arlZr1brtYb9abjZbzbbrY77a7nZ73b7vYH/aHg5Hw7Ho4n8djk5n47ns4X86Xi5Xy7Xq43663m53273u4P+6Ph5Px7Pp4v56vl5v17vt4f96fj5fz7fr4/76/n5/37/v4A/8gMAkDgLA0CIPAqDJGnaC4MghD4KQxCUOQtDUIw9CsMwnDsLw3CCPwojCJI4jv1g0jKLIqiaOoujaIY+imMYljmLY1iOPYrjOJ47i+N4gT+KEwSROEsTRIk8SpMkmTpLk2SFPkpTFJU5S1NUjT1K0zSdO02kKL03SjMMkzjLM0yLPMqzLJs6y7Nshz7KcxyXOctzXI89yvM8nzvL83yAv8oLApC4KwtCiLwqiyKYuiuLYoStyDPilLErS1KMvSrLMpy7K8tygr8qKwqSuKsqROS8qqtKmrqrq2qGvqprGpa5q2tajr2q6zqeu6vreoG/qhsGkbhrG0aJvGqbJpm6a5tmhb5qWxaVuWmBKtWza1q2nbtr23aDv2o7DpO46ztOi7zquy6buuu7boe+6nsel7nre16Pver7Pp+76/t+gH/pVQGQaB0HwbByGIehqHYZh+G4cRhHkaR1GUfRtHMYx7GsdxnH8bxwmCeJonSZJ8mycpinqap2mafpunGYZ5mmdZln2bZzmOe5kwNq5/mecFgXhaF0WRfFsXJYl6WpdlmX5blxWFeVpXVZV9W1c1jXta13Wdf1vXDYNxE+aNs3jfNy2Letq3bZt+27cdh3nad12Xfdt3PY972vd9n3/b9wOA+DoPQ5D8OStNiPo7D2OY/juPE4T5Ok9TlP07Tpko8zjPc5z/O88Lgvi6L0uS/LsvK4r6uq9rmv67rxuG+bpvW5b9u287jvu/u7Ou/7nvB4H4eh9Hkfx7HyeJ+nqfZ5n+e58Xhfl6X1eV/XtfN437et93nf973w+D+Po/T5P8+z8vi/r5Wvur/vm/H4f1cgA";
 
 global.btoa = function (str) {return new Buffer(str).toString('base64');};
 
@@ -91,13 +92,13 @@ describe('JSBloom', function() {
 
     describe('Import & Export', function() {
         it('should return true on predefined element in imported array', function() {
-            filter.importData("AwGl7SOrZ+6IcpqXrZj2u5/vQg4o0k8syi6q2m+uxh5p1l9tzj7r3n/voIHChokeLGSJ0qbJny5ihcqWqV6tZo3atunfr2GDxo6ZPmzli9au2b9u44fOnrl+7eeP3r75/+/QIDgoNCQ8LDIiOio2Jj4uMSE5KTUlPS0zIzsrNyc/LzCguKi0pLyssqK6qramvq6xobmptaW9rbOju6u3p7+vsGB4aHRkfGxyYnpqdmZ+bnFheWl1ZX1tc2N7a3dnf29w4OZgEYj8+OLq8ub67vbh/unx5fnt9eP96/Pn++/34B/yBgJBwLBoIh4KhkJh0LhsIR8KRiJRyLRqIx6KxmJx2LxuIJ+KJhJJxLJpIp5KplJp1LptIZ9KZjJZzLZrI57K5nJ53L5vIF/KFgpFwrFool4qlkpl0rlsoV8qVvzOirVyo16q1mp12r1uoN+qNhpNxrNpot5qtlpt1rttod9qdjpdzrdro97q9qVVnr93oD/qDgZDwbDoYj4ajNF9kbj0YT8aTiZTybTqYz6azmZz2bzuYL+aLhZLxbLpYr5arlZr1brtYb9abjZbzbbrY77a7nZ73b7vYH/aHg5Hw7Ho4n8djk5n47ns4X86Xi5Xy7Xq43663m53273u4P+6Ph5Px7Pp4v56vl5v17vt4f96fj5fz7fr4/76/n5/37/v4A/8gMAkDgLA0CIPAqDJGnaC4MghD4KQxCUOQtDUIw9CsMwnDsLw3CCPwojCJI4jv1g0jKLIqiaOoujaIY+imMYljmLY1iOPYrjOJ47i+N4gT+KEwSROEsTRIk8SpMkmTpLk2SFPkpTFJU5S1NUjT1K0zSdO02kKL03SjMMkzjLM0yLPMqzLJs6y7Nshz7KcxyXOctzXI89yvM8nzvL83yAv8oLApC4KwtCiLwqiyKYuiuLYoStyDPilLErS1KMvSrLMpy7K8tygr8qKwqSuKsqROS8qqtKmrqrq2qGvqprGpa5q2tajr2q6zqeu6vreoG/qhsGkbhrG0aJvGqbJpm6a5tmhb5qWxaVuWmBKtWza1q2nbtr23aDv2o7DpO46ztOi7zquy6buuu7boe+6nsel7nre16Pver7Pp+76/t+gH/pVQGQaB0HwbByGIehqHYZh+G4cRhHkaR1GUfRtHMYx7GsdxnH8bxwmCeJonSZJ8mycpinqap2mafpunGYZ5mmdZln2bZzmOe5kwNq5/mecFgXhaF0WRfFsXJYl6WpdlmX5blxWFeVpXVZV9W1c1jXta13Wdf1vXDYNxE+aNs3jfNy2Letq3bZt+27cdh3nad12Xfdt3PY972vd9n3/b9wOA+DoPQ5D8OStNiPo7D2OY/juPE4T5Ok9TlP07Tpko8zjPc5z/O88Lgvi6L0uS/LsvK4r6uq9rmv67rxuG+bpvW5b9u287jvu/u7Ou/7nvB4H4eh9Hkfx7HyeJ+nqfZ5n+e58Xhfl6X1eV/XtfN437et93nf973w+D+Po/T5P8+z8vi/r5Wvur/vm/H4f1cgA");
+            filter.importData(importData);
 
             assert.equal(filter.checkEntry("5a4f3bdf-476c-45e9-bd78-ab13ccc47255"), true);
         });
 
-        it('should return valid base64 output on export', function() {
-            assert.equal(typeof btoa(filter.exportData()), "string");
+        it('should return expected output on export', function() {
+            assert.equal(filter.exportData(), importData);
         });
     });
 });
